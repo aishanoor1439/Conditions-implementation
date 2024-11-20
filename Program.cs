97f18e8a2c6608ffe9f31eeadeb5c3c82dd620b0
aishanoor1439 <aisha.noor1439@gmail.com>
2024-11-20 10:27:56 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditions_implementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Taking input from the user
+            Console.WriteLine("Enter value for x:");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter value for y:");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter value for z:");
+            int z = int.Parse(Console.ReadLine());
+
+            // Condition: x > y > z
+            if (x > y && y > z)
+            {
+                Console.WriteLine("Condition a) x > y > z is true.");
+            }
+            else
+            {
+                Console.WriteLine("Condition a): x > y > z is false.");
+            }
+
+            // Condition: x and y both are less than 0
+            if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Condition b) is true. Both x and y are less than 0.");
+            }
+            else if (x >= 0)
+            {
+                Console.WriteLine("x is not less than 0. Hence, Condition b) is false.");
+
+            }
+            else if (y >= 0)
+            {
+                Console.WriteLine("y is not less than 0. Hence, Condition b)  is false.");
+            }
+            else
+            {
+                Console.WriteLine("Condition b) is false. Both x and y are greater than 0.");
+            }
+           
+
+            // Condition: Neither x nor y is less than 0
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Condition c) is true. Neither x nor y is less than 0.");
+            }
+               else if (x < 0)
+                {
+                    Console.WriteLine("x is less than 0. Hence, Condition c) is false.");
+                }
+               else if (y < 0)
+                {
+                    Console.WriteLine("y is less than 0. Hence, Condition c) is false.");
+                }
+             else
+            {
+                Console.WriteLine("Condition c) is false. Both x and y are less than 0.");
+            }
+
+            // Condition: x is equal to y but not equal to z
+            if (x == y && x != z)
+            {
+                Console.WriteLine("Condition d) is true. x is equal to y but not equal to z.");
+            }
+                else if(x != y)
+                {
+                    Console.WriteLine("x is not equal to y. Hence, Condition d) is false.");
+                }
+                else if(x == z)
+                {
+                    Console.WriteLine("x is equal to z. Hence, Condition d) is false.");
+                }
+            else
+            {
+                Console.WriteLine("Condition d) is false. x equal to z but not eaual to y.");
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Conditions implementation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Conditions implementation")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7107e1b2-b39b-40ab-a9ef-9c5cc34c6ee1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
